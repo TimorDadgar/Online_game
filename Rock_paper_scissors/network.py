039c8fd6ffef5ac06559b06ff8772da09bba54c1
timdah181 <timdah181@studentmail.oru.se>
2021-12-15 16:37:02 +0100

    added graphics and game

--- Rock_paper_scissors/network.py
+++ Rock_paper_scissors/network.py
@@ -1,46 +1,29 @@
-# Responsibility is to connect to a server
-
 import socket
-# will allow for serialized objects, objects made into bytes
 import pickle
 
+
 class Network:
-	def __init__(self):
-		self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-		self.server = "192.168.43.172"
-		self.port = 5555
-		self.addr = (self.server, self.port)
-		# This is implemented so that each player (client) will have their own id
-		self.p = self.connect()
+    def __init__(self):
+        self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.server = "192.168.43.172"
+        self.port = 5555
+        self.addr = (self.server, self.port)
+        self.p = self.connect()
 
-	def getP(self):
-		return self.p
+    def getP(self):
+        return self.p
 
-	def connect(self):
-		try:
-			self.client.connect(self.addr)
-			# Not using pickle anymore since we only returning 1 or 0
-			return self.client.recv(2048).decode()
-		except socket.error as e:
-			print(e)
-			pass
+    def connect(self):
+        try:
+            self.client.connect(self.addr)
+            return self.client.recv(2048).decode()
+        except:
+            pass
 
-	def send(self, data):
-		# If server cant receive message
-		try:
-			# pickle.dumps is the same as encoding the data
-			self.client.send(str.encode(data))
-			# This implementation of a recv at each send
-			# works in cases where you dont do pings
-			# where all send messages want a block reply
-			return pickle.loads(self.client.recv(2048))
-		except socket.error as e:
-			print(e)
+    def send(self, data):
+        try:
+            self.client.send(str.encode(data))
+            return pickle.loads(self.client.recv(2048*2))
+        except socket.error as e:
+            print(e)
 
-	# Recv function isn't really neded now since send function is doing recv.
-	def recv(self):
-		try:
-			reply = pickle.loads(self.client.recv(2048))
-			print(reply)
-		except socket.error as e:
-			print(e)
--- Rock_paper_scissors/server.py
+++ Rock_paper_scissors/server.py
@@ -1,7 +1,5 @@
-from os import error
 import socket
 from _thread import *
-import sys
 import pickle
 from game import Game
 
@@ -18,7 +16,6 @@ except socket.error as e:
 # Backlog parameter (2), specifies how many unaccepted connections are allowed 
 s.listen(2)
 # Implemented so I can get my KeyboardInterrup Ctrl+c through
-s.settimeout(10.0)
 print("waiting for a connection, Server Started")
 
 # Store addresses of connected clients
@@ -35,48 +32,57 @@ def threaded_client(conn, p, gameId):
 
 	reply = ""
 	while True:
-		data = conn.recv(4096).decode()
-		# Checks if game is still in dict. of games
-		if gameId in games:
-			game = games[gameId]
-		
-			if not data:
-				break
-			else:
-				if data == "reset":
-					game.resetWent()
-				elif data != "get":
-					game.play(p, data)
+		try:
+			data = conn.recv(4096).decode()
+			# Checks if game is still in dict. of games
+			if gameId in games:
+				game = games[gameId]
+			
+				if not data:
+					break
+				else:
+					if data == "reset":
+						game.resetWent()
+					elif data != "get":
+						game.play(p, data)
 
-				reply = game
-				conn.sendall(pickle.dumps(reply))
+					conn.sendall(pickle.dumps(game))
+			else:
+				break
+		except:
+			break
+
+	print("Lost connection")
+	try:
+		del games[gameId]
+		print("Closing Game", gameId)
+	except:
+		pass
+	idCount -= 1
+	conn.close
 
 
 # Continously looks for connections
 while True:
 	# Implemented so I can get my KeyboardInterrup Ctrl+c through
-	try:
-		# conn is an object that represents what is connected
-		# addr is an ipv4
-		conn, addr = s.accept()
-		print("Connected to:", addr)
-		# Keep track of how many people are connected at once
-		idCount += 1
-		p = 0
-		# // is integer division
-		# How many games do we have, if 7 people we need 4 games
-		gameId = (idCount - 1)//2
-		# Dont have enough players for game
-		if idCount % 2 == 1:
-			# Filling dict. with games
-			games[gameId] = Game(gameId)
-			print("Creating a new game...")
-		# Don't need to create a new game
-		else:
-			games[gameId].ready = True
-			p = 1
+	# conn is an object that represents what is connected
+	# addr is an ipv4
+	conn, addr = s.accept()
+	print("Connected to:", addr)
+	# Keep track of how many people are connected at once
+	idCount += 1
+	p = 0
+	# // is integer division
+	# How many games do we have, if 7 people we need 4 games
+	gameId = (idCount - 1)//2
+	# Dont have enough players for game
+	if idCount % 2 == 1:
+		# Filling dict. with games
+		games[gameId] = Game(gameId)
+		print("Creating a new game...")
+	# Don't need to create a new game
+	else:
+		games[gameId].ready = True
+		p = 1
 
-		start_new_thread(threaded_client, (conn, p, gameId))
-
-	except TimeoutError as e:
-		print("No connections were received, error:", e)
+	start_new_thread(threaded_client, (conn, p, gameId))
